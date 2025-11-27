cf24b42af1ae75c4eed794348721b5dd91cf3190
Govind-Shukla-Space <govindshukla8115@gmail.com>
2025-11-27 19:57:49 +0530

    intial

---
+++ src/main/java/com/store/jewellry/JewellryApplication.java
@@ -0,0 +1,13 @@
+package com.store.jewellry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JewellryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JewellryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/store/jewellry/JewellryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.store.jewellry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JewellryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
